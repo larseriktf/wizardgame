0c966632d94dda87bc8552a4807849d9c995d03f
Lars EriK <Lars.e.faber@gmail.com>
2021-04-11 14:56:45 +0200

    Added all essential project types

---
+++ WizardGame/WizardGame.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using WizardGame.App.Helpers;
+
+namespace WizardGame.App.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ WizardGame/WizardGame.App/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+
+using WizardGame.App.ViewModels;
+
+namespace WizardGame.App.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
