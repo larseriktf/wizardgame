b62974dd513072ab6588053d04523b8516bd938e
Lars EriK <Lars.e.faber@gmail.com>
2021-04-11 16:12:19 +0200

    Game now works again

--- WizardGame/WizardGame.App/Views/MainPage.xaml.cs
+++ WizardGame/WizardGame.App/Views/MainPage.xaml.cs
@@ -29,7 +29,7 @@ using WizardGame.App.Interfaces;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace WizardGame.App
+namespace WizardGame.App.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
