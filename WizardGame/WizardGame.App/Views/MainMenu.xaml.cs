21e662ca5c7354dbe024710dea178a1729a2f9ae
Lars EriK <Lars.e.faber@gmail.com>
2021-04-27 17:28:57 +0200

    Added MainMenu.Xaml and Styling

---
+++ WizardGame/WizardGame.App/Views/MainMenu.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+
+using WizardGame.App.ViewModels;
+
+namespace WizardGame.App.Views
+{
+    public sealed partial class MainMenu : Page
+    {
+        public BlankViewModel ViewModel { get; } = new BlankViewModel();
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_GettingFocus(Windows.UI.Xaml.UIElement sender, Windows.UI.Xaml.Input.GettingFocusEventArgs args)
+        {
+
+        }
+    }
+}
