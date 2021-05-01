f5c00796bff586e1ebdefbbef2be6cfae61f3d4e
Lars EriK <Lars.e.faber@gmail.com>
2021-05-01 19:09:00 +0200

    Added dependencies to Model and DataAccess projects. Updated Configuration.cs

--- WizardGame/WizardGame.Model/Configuration.cs
+++ WizardGame/WizardGame.Model/Configuration.cs
@@ -1,71 +1,35 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WizardGame.Model
 {
     public class Configuration
     {
-        // General Settings
-        private int volume = 50;
-        public int Volume
-        {
-            get
-            {
-                return volume;
-            }
-            set
-            {   // Making sure volume does not go above 100 or below 0
-                if (value > 100)
-                {
-                    value = 100;
-                }
-                else if (value < 0)
-                {
-                    value = 0;
-                }
-                volume = value;
-            }
-        }
+        [Key]
+        public int Id { get; set; }
 
-        
-        private int displayMode = 0;
-        public int DisplayMode
-        {   // 0 = Winowed, 1 = Windowed Borderless, 2 = Fullscreen
-            get
-            {
-                return displayMode;
-            }
-            set
-            {   // Making sure volume does not go above 2 or below 0
-                if (value > 2)
-                {
-                    value = 2;
-                }
-                else if (value < 0)
-                {
-                    value = 0;
-                }
-                displayMode = value;
-            }
-        }
+        // General Settings
+        public int Volume { get; set; } = 50;
+        [Required]
+        public int DisplayMode { get; set; } = 0; // 0 = Winowed, 1 = Windowed Borderless, 2 = Fullscreen
 
         // Keybindings
-        public char NavContinue { get; set; } = 'W';
-        public char NavPause { get; set; } = 'W';
-        public char NavBack { get; set; } = 'W';
+        public string NavContinue { get; set; } = "ENTER";
+        public string NavPause { get; set; } = "ESCAPE";
+        public string NavBack { get; set; } = "BACKSPACE";
 
-        public char MoveUp { get; set; } = 'W';
-        public char MoveLeft { get; set; } = 'A';
-        public char MoveDown { get; set; } = 'S';
-        public char MoveRight { get; set; } = 'D';
+        public string MoveUp { get; set; } = "W";
+        public string MoveLeft { get; set; } = "A";
+        public string MoveDown { get; set; } = "S";
+        public string MoveRight { get; set; } = "D";
 
-        public char Action1 { get; set; } = 'U';
-        public char Action2 { get; set; } = 'R';
-        public char Action3 { get; set; } = 'D';
-        public char Action4 { get; set; } = 'L';
+        public string Action1 { get; set; } = "UPARROW";
+        public string Action2 { get; set; } = "RIGHTARROW";
+        public string Action3 { get; set; } = "DOWNARROW";
+        public string Action4 { get; set; } = "LEFTARROW";
 
-        public char Interact1 { get; set; } = 'L';
-        public char Interact2 { get; set; } = 'L';
-        public char Interact3 { get; set; } = 'L';
+        public char Interact1 { get; set; } = 'R';
+        public char Interact2 { get; set; } = 'F';
+        public char Interact3 { get; set; } = 'C';
 
     }
 }
