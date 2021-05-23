3d1c99650679a04c70100b9323ef8b2f699e0270
Lars EriK <Lars.e.faber@gmail.com>
2021-05-23 17:00:02 +0200

    Added more functionality to PlayerProfilePage

--- WizardGame/WizardGame.App/ViewModels/PlayerProfilePageViewModel.cs
+++ WizardGame/WizardGame.App/ViewModels/PlayerProfilePageViewModel.cs
@@ -1,13 +1,41 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using WizardGame.App.Core.Services;
 using WizardGame.App.Helpers;
+using WizardGame.Model;
 
 namespace WizardGame.App.ViewModels
 {
     public class PlayerProfilePageViewModel : Observable
     {
-        public PlayerProfilePageViewModel()
+        public ObservableCollection<PlayerProfile> PlayerProfiles { get; set; } = new ObservableCollection<PlayerProfile>();
+        private HttpDataService dataService = new HttpDataService();
+
+        internal async Task LoadAllPlayerProfilesAsync()
         {
+            List<PlayerProfile> configurations = new List<PlayerProfile>()
+            {
+                await dataService.GetAsync<PlayerProfile>("http://localhost:34367/api/PlayerProfiles/1"),
+                await dataService.GetAsync<PlayerProfile>("http://localhost:34367/api/PlayerProfiles/2"),
+                await dataService.GetAsync<PlayerProfile>("http://localhost:34367/api/PlayerProfiles/3")
+            };
+
+            foreach (PlayerProfile playerProfiles in configurations)
+            {   // Add configurations to observable collection
+                PlayerProfiles.Add(playerProfiles);
+            }
+        }
+
+        internal async Task AddNewPlayerProfileAsync(string playerName)
+        {
+            PlayerProfile playerProfile = new PlayerProfile()
+            {
+                PlayerName = "playerName"
+            };
+
+            await dataService.PostAsync<PlayerProfile>("http://localhost:34367/api/PlayerProfiles/", playerProfile);
         }
     }
 }
--- WizardGame/WizardGame.App/Views/PlayerProfilePage.xaml.cs
+++ WizardGame/WizardGame.App/Views/PlayerProfilePage.xaml.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
 using WizardGame.App.ViewModels;
 
 namespace WizardGame.App.Views
 {
     public sealed partial class PlayerProfilePage : Page
     {
-        public PlayerProfilePageViewModel ViewModel { get; } = new PlayerProfilePageViewModel();
+        public PlayerProfilePageViewModel PlayerProfileViewModel { get; } = new PlayerProfilePageViewModel();
 
         public PlayerProfilePage()
         {
             InitializeComponent();
+        }
+
+        private async void OnLoadedAsync(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await PlayerProfileViewModel.LoadAllPlayerProfilesAsync();
+        }
+
+        private void OnToggleProfileCreationMenu(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (ProfileCreationMenu.Visibility == Visibility.Visible)
+            {
+                ProfileCreationMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ProfileCreationMenu.Visibility = Visibility.Visible;
+            }
+        }
+
+        private async void OnAddPlayerProfileAsync(object sender, RoutedEventArgs e)
+        {
+            OnToggleProfileCreationMenu(sender, e);
+            await PlayerProfileViewModel.AddNewPlayerProfileAsync("heyo");
         }
     }
 }
